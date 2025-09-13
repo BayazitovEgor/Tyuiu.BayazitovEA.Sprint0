a4e12ee8715162a1365e1167065a214c15642b31
Егор Баязитов <Tyuiu.BayazitovEA@outlook.com>
2025-09-13 20:25:57 +0500

    Task1

---
+++ Tyuiu.BayazitovEA.Sprint0.Task1.V0.Test/Test1.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.BayazitovEA.Sprint0.Task1.V0.Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tyuiu.BayazitovEA.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,11 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        string name = "Егор";
+        int age = 18;
+        Console.WriteLine("Здраствуйте меня зовут " + name + " мне " + age + " лет!");
+        Console.ReadKey();
+    }
+}
