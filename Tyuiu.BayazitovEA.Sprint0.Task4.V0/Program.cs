9a3036d8beab8cf6d05790e501ca28c1956b4493
Егор Баязитов <Tyuiu.BayazitovEA@outlook.com>
2025-09-16 22:06:56 +0500

    Task4

---
+++ Tyuiu.BayazitovEA.Sprint0.Task4.V0/Program.cs
@@ -0,0 +1,11 @@
+﻿using Tyuiu.BayazitovEA.Sprint0.Task4.V0.Lib;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(DataService.Addition(1,5));
+        Console.WriteLine(DataService.Subtraction(1, 5));
+        Console.WriteLine(DataService.Multiplication(1, 5));
+        Console.WriteLine(DataService.Division(1, 5));
+    }
+}
